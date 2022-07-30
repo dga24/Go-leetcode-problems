973ed7d81ee508c4b665f2650c032328885b7807
dga24 <garciadavid.24@gmail.com>
2022-07-30 20:28:02 +0200

    cirlce ok

---
+++ mergeTwoLists/main.go
@@ -0,0 +1,33 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeTwoLists(l1 *ListNode, l2 *ListNode) *ListNode {
+	var newList = &ListNode{}
+	var out = newList
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			newList.Next = l1
+			l1 = l1.Next
+			newList = newList.Next
+		} else {
+			newList.Next = l2
+			l2 = l2.Next
+			newList = newList.Next
+		}
+	}
+	if l1 != nil {
+		newList.Next = l1
+	} else if l2 != nil {
+		newList.Next = l2
+	}
+
+	return out.Next
+}
